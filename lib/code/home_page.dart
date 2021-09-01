7de14c80ef2718531eb071307761cadf65e39787
Bicky Verma <bickyvermamsp420@gmail.com>
2021-09-01 21:03:07 +0530

    Day 4

--- lib/code/home_page.dart
+++ lib/code/home_page.dart
@@ -1,22 +1,65 @@
 
-
+import 'package:velocity_x/velocity_x.dart';
+import 'package:catalogapp/code/themes.dart';
+import 'package:catalogapp/models/catalog.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'catalog_header.dart';
+import 'catalog_list.dart';
 
-import 'drawer.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatefulWidget{
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson = await rootBundle.loadString(
+        "assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {
+
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Catalog App"),
-      ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to the Flutter"),
-        ),
-      ),
-      drawer: MyDrawer(),
-    );
+        backgroundColor: MyTheme.creamColor,
+        body: SafeArea(
+            child: Container(
+              padding: EdgeInsets.all(32),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CatalogHeader(),
+                  if(CatalogModel.items != null &&
+                      CatalogModel.items.isNotEmpty)
+                    CatalogList().py16().expand()
+                  else
+                    CircularProgressIndicator().centered().expand(),
+                ],
+              ),
+            )
+        ));
   }
 }
+
+
+
+
--- lib/code/themes.dart
+++ lib/code/themes.dart
@@ -3,7 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 
  class MyTheme{
   static ThemeData lightTheme(BuildContext context) =>  ThemeData(primarySwatch: Colors.deepPurple,
- fontFamily: GoogleFonts.lato().fontFamily,
+ fontFamily: GoogleFonts.poppins().fontFamily,
  appBarTheme: AppBarTheme(
  color: Colors.white,
  elevation: 0.0,iconTheme: IconThemeData(color: Colors.black),
@@ -11,4 +11,7 @@ import 'package:google_fonts/google_fonts.dart';
  ));
   static ThemeData darkTheme(BuildContext context) =>  ThemeData(brightness: Brightness.dark,
       );
+  static Color creamColor = Color(0xfff5f5f5);
+  static Color darkBluishColor = Color(0xff403b58);
+
  }
